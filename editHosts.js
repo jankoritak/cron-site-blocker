752162877f9f8ddc8621ea3da78a83203e5210b1
Jan Koriťák <koritakjan@gmail.com>
2024-10-04 10:48:54 +0200

    fix: correct line string

--- editHosts.js
+++ editHosts.js
@@ -3,6 +3,9 @@ const os = require("os");
 
 const HOSTS_FILE_PATH = "/etc/hosts";
 
+const HOST_WWW_PREFIX = "www.";
+const HOST_LOCAL_HOST = "127.0.0.1";
+
 const BLOCKED_HOSTS_LIST = ["youtube.com", "reddit.com"];
 
 const addHostsEntry = () => {
@@ -11,8 +14,10 @@ const addHostsEntry = () => {
 
     BLOCKED_HOSTS_LIST.forEach((blockedHost) => {
       if (!hostsFile.includes(blockedHost)) {
-        hostsFile += blockedHost + os.EOL;
-        fs.writeFileSync(HOSTS_FILE_PATH, hostsFile, "utf8");
+        hostsFile += HOST_LOCAL_HOST + " " + blockedHost + os.EOL;
+        hostsFile +=
+          HOST_LOCAL_HOST + " " + HOST_WWW_PREFIX + blockedHost + os.EOL;
+        fs.writeFileSync(HOSTS_FILE_PATH, hostsFile.trim(), "utf8");
 
         console.log("Added", blockedHost, "to", HOSTS_FILE_PATH);
       } else {
@@ -30,8 +35,12 @@ const removeHostsEntry = () => {
 
     BLOCKED_HOSTS_LIST.forEach((blockedHost) => {
       if (hostsFile.includes(blockedHost)) {
-        hostsFile = hostsFile.replace(blockedHost, "");
-        fs.writeFileSync(HOSTS_FILE_PATH, hostsFile, "utf8");
+        hostsFile = hostsFile.replace(HOST_LOCAL_HOST + " " + blockedHost, "");
+        hostsFile = hostsFile.replace(
+          HOST_LOCAL_HOST + " " + HOST_WWW_PREFIX + blockedHost,
+          ""
+        );
+        fs.writeFileSync(HOSTS_FILE_PATH, hostsFile.trim(), "utf8");
 
         console.log("Removed", blockedHost, "from", HOSTS_FILE_PATH);
       } else {
